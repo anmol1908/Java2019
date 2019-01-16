38fdbef6fd6f104b784f87b07ecf305f343ddcb9
anmol nagpal <anmol.nagpal1908@gmail.com>
2019-01-16 22:32:26 +0530

    added til lecture 4

--- lecture4/inclasspractice/programme3/src/Sort.java
+++ lecture4/inclasspractice/programme3/src/Sort.java
@@ -2,53 +2,57 @@ import java.util.Scanner;
 
 class Sort
 {
-public static void main(String [] args)
-{
-Scanner scan=new Scanner(System.in);
-int l1,l2,i=0,j=0,k=0,count;
-// asking user to give the size of both arrays
-/*System.out.println("Enter number of elements in array 1 and 2 respectively :  ");
-l1=scan.nextInt();
-l2=scan.nextInt();*/
-int arr1[]=new int[4], arr2[]=new int[5],arr3[]=new int[9];
-System.out.println("\n Enter elements in array1 :  ");
-for( count=0;count<arr2.length;count++)
-{
-arr1[count]=scan.nextInt();
-}
-for( count=0;count<l2;count++)
-{
-arr2[count]=scan.nextInt();
-}
-while(i<arr1.length && j<arr2.length)
-{
-if(arr1[i]<arr2[j])
-{
-arr3[k]=arr1[i];
-++i;
-}
-else
-{
-arr3[k]=arr2[j];
-++j;
-}
-++k;
-}
-while(k<=i+j)
-{
-if(l1>l2)
-{
-arr3[k]=arr1[i];
-}
-else
-{
-arr3[3]=arr2[j];
-}
-}
-System.out.println("\n Merged and sorted array so formed is :   ");
-for( count=0;count<l1+l2;++count)
-{
-System.out.print(+arr3[count]+"\t");
-}
-}
+ public static void main(String [] args)
+ {
+   	Scanner scan=new Scanner(System.in);
+	int l1,l2,i=0,j=0,k=0,count;
+	// asking user to give the size of both arrays
+	System.out.println("Enter number of elements in array 1 and 2 respectively :  ");
+	l1=scan.nextInt();
+	l2=scan.nextInt();
+	//array declaration 
+	int arr1[]=new int[l1], arr2[]=new int[l2],arr3[]=new int[l1+l2];
+	System.out.println("\n Enter elements in array1 :  ");
+		for( count=0;count<l1;count++)
+		{
+		arr1[count]=scan.nextInt();
+		}
+		 for( count=0;count<l2;count++)
+		 {
+		 arr2[count]=scan.nextInt();
+		 }                                         
+		    while(i<arr1.length && j<arr2.length)
+		    {
+			if(arr1[i]<arr2[j])
+			{
+			arr3[k]=arr1[i];               //if array 1 has smaller element then it is pushed to array 3
+			++i;
+			}
+			else
+			{
+			arr3[k]=arr2[j];		//if array 2 has smaller element then it is pushed to array 3
+			++j;
+			}
+			++k;
+		     }
+			   while(k<=i+j)		//loop exists till last index number of array3
+			   {
+				if(l1>l2)
+				{
+				arr3[k]=arr1[i];        // if array 1 has some elements left then they are pushed to arry 3
+				++i 
+				}
+				else
+				{
+				arr3[3]=arr2[j];	// if array 2 has some elements left then they are pushed to arry 3
+				++j
+				}
+			   ++k
+ 			   }
+	System.out.println("\n Merged and sorted array so formed is :   ");
+	for( count=0;count<l1+l2;++count)
+	{
+	System.out.print(+arr3[count]+"\t");
+	}
+ }
 }
