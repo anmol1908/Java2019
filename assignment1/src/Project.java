1b04953e1d246f4e46877e27d406cc73a44feee3
anmol nagpal <anmol.nagpal1908@gmail.com>
2019-02-03 22:57:52 +0530

    final codes uploaded

--- assignment1/src/Project.java
+++ assignment1/src/Project.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Project
 {
 String name;
@@ -6,18 +7,39 @@ Date endDate;
 String role;
 String [] responsibilities;
 
+Scanner scan=new Scanner(System.in);
+
+
+Project()
+{
+this.name=null;
+this.role=null;
+}
+
+
 Project(String n,String role)
 {
-	this.name=name;
-	this.role=role;
+this.name=name;
+this.role=role;
 }
+
+
 void setStartDate (int d,int m, int y)
-	{
-		startDate.setValueDate(d,m,y);	
-	}
+{
+startDate=new Date(d,m,y);	
+}
 
 void setEndDate (int d,int m, int y)
-	{
-		endDate.setValueDate(d,m,y);	
-	}
+{
+ endDate=new Date(d,m,y);	
+}
+
+void responsiblities(String res)
+{
+System.out.println("  how many responsiblities does the student have ? enter number ");
+int num=scan.nextInt();
+System.out.println(" Enter responsiblities ");
+for(int i=0;i<num;++i)
+this.responsibilities[i]=res;
+}
 }
--- assignment1/src/Qualification.java
+++ assignment1/src/Qualification.java
@@ -4,6 +4,13 @@ String qualName;
 String university;
 String institute;
 float cgpa;
+	Qualification()
+	{
+		this.qualName = null;
+		this.university = null;
+		this.institute = null;
+		this.cgpa = 0;
+	}
 	Qualification(String q, String u, String i, float c)
 	{
 		this.qualName = q;
